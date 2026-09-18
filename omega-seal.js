c0ab65c819b30d96a3169b352ab2fe713fe747b8
ObsidianSeal <bmgosselin@gmail.com>
2026-09-18 00:19:32 -0400

    it's not me it's you

--- omega-seal.js
+++ omega-seal.js
@@ -219,7 +219,7 @@ client.on("interactionCreate", async (interaction) => {
 				}
 			}
 
-			replyText = `## :station: ION arrivals :station:\n-# to University of Waterloo Station\n- SOUTHBOUND: not in service\n- NORTHBOUND: not in service\n-# please tell me if you believe there is an error`;
+			replyText = `## :station: ION arrivals :station:\n-# to University of Waterloo Station\n- SOUTHBOUND: not in service\n- NORTHBOUND: not in service\n-# please confirm with GRT before reporting this as an error`;
 
 			if (southboundTime != Infinity && northboundTime != Infinity) {
 				replyText = `## :station: ION arrivals :station:\n-# to University of Waterloo Station\n- SOUTHBOUND: <t:${southboundTime}:R> (${formatTime(new Date(southboundTime * 1000))})\n- NORTHBOUND: <t:${northboundTime}:R> (${formatTime(
